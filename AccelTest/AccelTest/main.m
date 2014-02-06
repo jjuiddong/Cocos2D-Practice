f5cf0950bb7a2d594b18214f7d9d4bedb6ae13a7
jjuiddong <jjuiddong@hanmail.net>
2014-02-07 08:42:02 +0900

    Add Acceleration Test Project

---
+++ AccelTest/AccelTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  AccelTest
+//
+//  Created by 재정 이 on 12. 4. 7..
+//  Copyright __MyCompanyName__ 2012년. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
